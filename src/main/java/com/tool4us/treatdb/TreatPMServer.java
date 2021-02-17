000ff5e90f79f49193702fbab3bdf89103ca7cb1
Kim, MyungHyun <mh9.kim@lge.com>
2021-02-17 17:02:13 +0900

    works at 2021-02-17

--- src/main/java/com/tool4us/treatdb/TreatPMServer.java
+++ src/main/java/com/tool4us/treatdb/TreatPMServer.java
@@ -14,53 +14,14 @@ import lib.turbok.task.TaskQueue;
 
 /**
  * Request Handling Server
- * 
- * @author mh9.kim
+ * @author TurboK
  */
-public class TreatPMServer
+public class TreatPMServer implements IStaticFileMap
 {
     private TomyServer      _serverBase = null;
-    
     private TaskQueue       _taskQueue = null;
-    
-    
-    private static class StaticFileMap implements IStaticFileMap
-    {
-        @Override
-        public String getRootFile()
-        {
-            return "index.html";
-        }
-        
-        @Override
-        public boolean isAllowed(String uriPath)
-        {
-            // 소스 보안을 위하여 *.map 파일은 반환하지 않음.
-            return !uriPath.endsWith(".map");
-        }
 
-        @Override
-        public File getFile(String uriPath)
-        {
-            String vDir = null;
-            int sPos = uriPath.indexOf("/", 1);
-
-            if( sPos != -1 )
-            {
-                vDir = uriPath.substring(1, sPos);
-                // vDir에 해당하는 실제 경로 찾기
-                vDir = OPT.virtualDir(vDir);
-            }
-            
-            if( vDir != null )
-                uriPath = uriPath.substring(sPos);
-            else
-                vDir = OPT.virtualRoot();
-            
-            return new File(vDir + uriPath);
-        }
-    }
-      
+    
     public TreatPMServer()
     {
         _taskQueue = new TaskQueue(null);
@@ -76,7 +37,7 @@ public class TreatPMServer
         // Secure 통신하려면 아래 주석 해제
         // System.setProperty("ssl", "true");
 
-        _serverBase = new TomyServer("com.tool4us.treatdb.service", new StaticFileMap());
+        _serverBase = new TomyServer("com.tool4us.treatdb.service", this);
         _serverBase.start(OPT.port(), OPT.bossThreadNum(), OPT.serviceThreadNum(), 0);
 
         _taskQueue.startQueue(2, "TreatDB Batch");
@@ -90,5 +51,39 @@ public class TreatPMServer
         _serverBase = null;
         
         _taskQueue.endQueue();
+    }
+    
+    @Override
+    public String getRootFile()
+    {
+        return "index.html";
+    }
+    
+    @Override
+    public boolean isAllowed(String uriPath)
+    {
+        // 소스 보안을 위하여 *.map 파일은 반환하지 않음.
+        return !uriPath.endsWith(".map");
+    }
+
+    @Override
+    public File getFile(String uriPath)
+    {
+        String vDir = null;
+        int sPos = uriPath.indexOf("/", 1);
+
+        if( sPos != -1 )
+        {
+            vDir = uriPath.substring(1, sPos);
+            // vDir에 해당하는 실제 경로 찾기
+            vDir = OPT.virtualDir(vDir);
+        }
+        
+        if( vDir != null )
+            uriPath = uriPath.substring(sPos);
+        else
+            vDir = OPT.virtualRoot();
+        
+        return new File(vDir + uriPath);
     }
 }
