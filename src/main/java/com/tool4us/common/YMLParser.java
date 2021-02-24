e3887fa18897b9bcf0114251756690466b7d46f5
Kim, MyungHyun <mh9.kim@lge.com>
2021-02-24 16:59:11 +0900

    works at 2021-02-24

--- src/main/java/com/tool4us/common/YMLParser.java
+++ src/main/java/com/tool4us/common/YMLParser.java
@@ -8,6 +8,8 @@ import java.util.Stack;
 
 import org.json.JSONObject;
 
+
+
 /**
  * Simple YML parser.
  * YML의 문법 중 다음만 지원함. 프로그램 실행 옵션을 YML 형태로 지정 시 사용 
@@ -47,61 +49,123 @@ public class YMLParser
     private JSONObject      _result = null;
     
     // 이전 단게의 상태 저장. [indent, element 상태(목록, 멀티라인, 싱글멀티라인), 현재까지 추가한 값]
-    private Stack<State>    _stack = null;
+    private Stack<Token>    _stack = null;
 
     
-    public static class State
+    public static class Token
     {
         public int _indent = 0;
-        public int _status = 0; // 0: 노말, 1: 목록, 2: 멀티라인, 3: 싱글멀티라인
+
+        /**
+         * 0: 아직 모름
+         * 1: 단순 값
+         * 2: 목록 (-)
+         * 3: 멀티라인 문자열 (|)
+         * 5: 멀티라인 문자열 (멀티 싱글라인 >) 
+         * 4: 객체
+         */
+        public int _type = 0;
+
+        public String _key = "";
         public String _value = "";
+        
+        public Token()
+        {
+            _indent = 0;
+            _type = 0;
+            _value = "";
+        }
+
+        public Token(int indent, int type, String key)
+        {
+            _indent = indent;
+            _type = type;
+            _key= key;
+        }
     }
 
     
     public YMLParser()
     {
         _result = new JSONObject();
-        _stack = new Stack<State>();
-
-        _stack.push(new State());
+        _stack = new Stack<Token>();
     }
     
-    public void putLineText(String lineText) throws ParseException
+    // p에 해당하는 문자가 Whitespace인지 여부 반환
+    public boolean isWhitespace(final String text, int p)
     {
-        int indent = 0;
-        while( Character.isWhitespace(lineText.charAt(indent)) )
-            indent += 1;
-        
-        State state = _stack.peek();
+        return p <= text.length() || Character.isWhitespace(text.charAt(p));
+    }
 
-        // 비거나 white space만 있는 문자열임.
-        if( indent >= lineText.length() )
+    // start부터 시작하여 Whitespace가 아닌 위치를 찾아 반환.
+    public int skipSpace(final String text, int start)
+    {
+        while( start < text.length() && Character.isWhitespace(text.charAt(start)) )
+            start += 1;
+        
+        return start;
+    }
+
+    public void pushLineText(final String lineText) throws ParseException
+    {
+        // --- 고려 해야 함.
+        if( lineText.startsWith("---") )
         {
-            // 멀티라인이면 CR 추가
-            if( state._status == 2 )
-                state._value += "\n";
-            // 싱글멀티라인이면 space 추가
-            else if( state._status == 3 )
-                state._value += " ";
-            
-            // 이외의 경우는 할 일 없음.
+            // 새로운 블럭의 시작임
+            _stack = new Stack<Token>();
             return;
         }
-
-        char ch = lineText.charAt(indent);
         
-        // 주석은 값을 따로 처리할 필요가 없음.
-        if( '#' == ch )
+        int p = skipSpace(lineText, 0);
+
+        Token curToken = _stack.isEmpty() ? null : _stack.peek();
+
+        // 빈 문자열이거나 화이트 스페이스만 있는 문자열임
+        if( p >= lineText.length() )
         {
-            return;
+            // |(CR) 나 >(공백) 로 멀티 라인이라면 추가하고
+            // 값이 와야 하는 경우인데 없다면 에러 --> 앞에 키만 있는 경우
+            // 이외의 경우는 무시
         }
-        else if( '-' == ch )
+        
+        int curIndent = p;
+        char ch = lineText.charAt(p);
+        
+        // 목록.
+        // 이전 키가 없으면 목록으로 이루어진 yml임.
+        // 이전 키가 있으면 목록 가능 여부, indent 체크
+        if( ch == '-' && isWhitespace(lineText, p + 1) )
+        {
+            
+        }
+
+        StringBuilder sbKey = new StringBuilder();
+        while( p < lineText.length() )
+        {
+            ch = lineText.charAt(p);
+        }
+        
+        // 
+        // 0: not defined
+        // 1: key-value
+        int prevToken = 0;
+        int prevIndent = 0;
+        
+        // prevIndent < indent --> 새로운 key 등장 필요
+        
+        // 주석 --> # ~
+        // 새로운 키의 시작 --> foo:
+        // 목록 --> -
+        // 문자열만 존재
+        
+        Token state = null;
+        
+        // 스택에 아무것도 없는 경우는 처음 객체가 필요한 경우임.
+        if( _stack.isEmpty() )
         {
             
         }
         
-        
-        //
     }
     
     public JSONObject getResult()
@@ -118,9 +182,10 @@ public class YMLParser
         String lineText = in.readLine();
         while( lineText != null )
         {
-            psr.putLineText(lineText);
+            psr.pushLineText(lineText);
+            lineText = in.readLine();
         }
-        
+
         in.close();
 
         return psr.getResult();
