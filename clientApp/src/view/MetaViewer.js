34a37a5f2d36cac7dd283a79e025d11c8c302a51
Kim, MyungHyun <mh9.kim@lge.com>
2021-03-19 19:26:23 +0900

    works at 2021-03-19

--- clientApp/src/view/MetaViewer.js
+++ clientApp/src/view/MetaViewer.js
@@ -13,10 +13,13 @@ import './MetaViewer.scss';
 
 
 const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
   typography: {
     // In Chinese and Japanese the characters are usually larger,
     // so a smaller fontsize may be appropriate.
-    fontSize: 12,
+    fontSize: 11,
   },
 });
 
